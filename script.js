429490a888b5f6b0b8217f0c8e4006d1744ed270
thiago-ulloa-develcode <thiago.ulloa@develcode.com.br>
2022-07-21 11:20:15 -0300

    Terminando menu lateral

--- script.js
+++ script.js
@@ -1,7 +1,5 @@
 function openNav() {
-    if (document.getElementById("mySidemenu").style.width = "6%") {
-        document.getElementById("mySidemenu").style.width = "15%";
-    } else {
-        document.getElementById("mySidemenu").style.width = "6%";
-    }
-  }
+  const sideMenu = document.getElementById("mySidemenu");
+
+  sideMenu.classList.toggle("sidemenu-open");
+}
